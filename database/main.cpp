5df81275e1b2257fc56e76009ac13b341c84f574
Nguyen Cao Ky <caoky2003xx@gmail.com>
2025-02-04 20:02:43 +0700

    fix null reference input buffer

--- database/main.cpp
+++ database/main.cpp
@@ -31,8 +31,10 @@ void read_input(InputBuffer *input_buffer) {
     }
 
     // Ignore trailing newline
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
+    if (input_buffer != nullptr) {
+        input_buffer->input_length = bytes_read - 1;
+        input_buffer->buffer[bytes_read - 1] = 0;
+    }
 }
 
 void close_input_buffer(InputBuffer *input_buffer) {
